69fecfa10b846b0cbe48bd414153e4701e41ff94
nameless-mc <s.taniguchi.zip@gmail.com>
2022-10-29 04:01:12 +0000

    setup.py の追加

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+def _requires_from_file(filename):
+    ls = open(filename).read().splitlines()
+    return list(filter(lambda x : not (len(x) == 0 or x.startswith('#')), ls))
+
+print(_requires_from_file('requirements.txt'))
+
+setup(
+    name = "yolov7"
+    install_requires=_requires_from_file('requirements.txt')
+)
