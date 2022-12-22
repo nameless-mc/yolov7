ce6c95d5482e2eb10849eaf4f1f3e14995f174b7
nameless-mc <s.taniguchi.zip@gmail.com>
2022-12-22 04:19:42 +0000

    setup の編集

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def _requires_from_file(filename):
 
 setup(
     name = "yolov7",
-    package_dir={"*": "yolov7"},
+    package_dir={"": "yolov7"},
     py_modules=[splitext(basename(path))[0] for path in glob.glob('*.py')],
     install_requires = _requires_from_file('requirements.txt')
 )
