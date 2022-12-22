0e39ecb27625a471099c9c25eccc0f8cb0ddff33
nameless-mc <s.taniguchi.zip@gmail.com>
2022-12-22 03:20:33 +0000

    setup の編集

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import glob
+from os.path import basename
+from os.path import splitext
 from setuptools import setup
 
 def _requires_from_file(filename):
@@ -7,5 +10,7 @@ def _requires_from_file(filename):
 
 setup(
     name = "yolov7",
+    package_dir={"": "."},
+    py_modules=[splitext(basename(path))[0] for path in glob.glob('*.py')],
     install_requires = _requires_from_file('requirements.txt')
 )
